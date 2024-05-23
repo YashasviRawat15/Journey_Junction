5e0db2ed8e1ecf9121bc5a6ee36b82e69bf38e86
YashasviRawat15 <103303642+YashasviRawat15@users.noreply.github.com>
2024-05-24 02:25:51 +0530

    worked on navbar and home

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,9 +1,39 @@
 import React from "react";
-import './home.css'
+import './home.scss'
+import video from '../../Assets/vid1.mp4'
+import { GrLocation } from "react-icons/gr";
 
 const Home = () => {
     return(
-        <div>Home</div>
+        <section className="home">
+            <div className="overlay"></div>
+                <video src={video} muted autoPlay loop typeof="video/mp4"></video>
+                
+                <div className="homeContent container">
+                    <div className="textDiv">
+
+                        <span className="smallText">
+                            Our Packages
+                        </span>
+
+                        <h1 className="homeTitle">
+                            Search your Holiday
+                        </h1>
+
+                    </div>
+
+                    <div className="cardDiv">
+                        <div className="destinationInput">
+                            <label htmlFor="city">Search your destination</label>
+                            <div className="input flex">
+                                <input type="text" placeholder="Enter name here....."/>
+                                <GrLocation className="icon"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+        </section>
     )
 }
 
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,33 @@
-import React from "react";
-import './navbar.css'
+import React, { useState } from "react";
+import './navbar.scss'
 import {MdTravelExplore} from 'react-icons/md'
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
+
+
 
 const Navbar = () => {
+
+    const [active, setActive] = useState('navBar')
+
+    const showNav = () => {
+        setActive('navBar activeNavbar')
+    }
+
+    const removeNav = () => {
+        setActive('navBar')
+    }
+
     return(
         <section className="navBarSection">
             <header className="header flex">
                 <div className="logoDiv">
                     <a href="#" className="logo flex">
-                        <h1> <MdTravelExplore className="icon"/> Journey Junction</h1>
+                        <h1> <MdTravelExplore className="icon"/> JourneyJunction</h1>
                     </a>
                 </div>
 
-                <div className="navBar">
+                <div className={active}>
                     <ul className="navLists flex">
                         <li className="navItem">
                             <a href="#" className="navLink">Home</a>
@@ -41,11 +56,22 @@ const Navbar = () => {
                         <li className="navItem">
                             <a href="#" className="navLink">Contact</a>
                         </li>
+
+                        <button className="btn">
+                            <a href="#">BOOK NOW</a> 
+                        </button>
                     </ul>
+                    <div onClick={removeNav} className="closeNavbar">
+                    <AiFillCloseCircle className="icon"/>
+                    </div>
+                    
                 </div>
+                <div onClick={showNav} className="toggleNavbar">
+                    <TbGridDots className="icon"/>
+                    </div>
             </header>
         </section>
     )
 }
 
-export default Navbar;
+export default Navbar; 
