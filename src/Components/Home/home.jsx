b58bd785af9dd2aff3ec1e52e225a9672c730368
YashasviRawat15 <103303642+YashasviRawat15@users.noreply.github.com>
2024-05-24 22:25:36 +0530

    Home page created

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import './home.scss'
 import video from '../../Assets/vid1.mp4'
 import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+import { BsListTask } from "react-icons/bs";
+import { TbApps } from "react-icons/tb";
 
 const Home = () => {
     return(
@@ -29,6 +35,43 @@ const Home = () => {
                                 <input type="text" placeholder="Enter name here....."/>
                                 <GrLocation className="icon"/>
                             </div>
+                        </div>
+
+                        <div className="dateInput">
+                            <label htmlFor="city">Search your date</label>
+                            <div className="input flex">
+                                <input type="date" />
+                                
+                            </div>
+                        </div>
+
+                        <div className="priceInput">
+                            <div className="label_total flex">
+                                <label htmlFor="price"> Max price:</label>
+                                <h3 className="total"> ₹5,00,000</h3>
+                            </div>
+                            <div className="input flex">
+                                <input type="range" max="500000" min="10000"/>
+                            </div>
+                        </div>
+
+                        <div className="searchOptions flex">
+                        <HiFilter className="icon"/>
+                        <span>MORE FILTERS</span> 
+                        </div>
+                    </div>
+
+                    <div className="homeFooterIcons flex">
+                        <div className="rightICons">
+                        <FiFacebook className="icon"/>
+                        <AiOutlineInstagram className="icon"/>
+                        <FaTripadvisor className="icon"/>
+                        </div>
+
+                        <div className="leftIcons">
+                        <BsListTask className="icon"/>
+                        <BsListTask className="icon"/>
+                        <TbApps className="icon"/>
                         </div>
                     </div>
                 </div>
